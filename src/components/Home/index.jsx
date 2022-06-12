0f3feadf35e154ff52c5f693bf3b70f844ec1df4
Omer Furkan <omerfurkansen@gmail.com>
2022-06-12 23:44:57 +0300

    feat: create async thunks and extra reducers

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,23 +1,14 @@
 import React, { useEffect } from "react";
 import MovieListing from "../MovieListing";
-import movieApi from "../../common/movieApi";
-import { OMDB_API_KEY } from "../../common/constants";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
 
 export default function Home() {
-  const movieText = "Harry";
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi.get(`?apikey=${OMDB_API_KEY}&s=${movieText}&type=movie`).catch((error) => {
-        console.log(error);
-      });
-      dispatch(addMovies(response.data.Search));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+  }, [dispatch]);
 
   return (
     <div>
--- src/features/movies/movieSlice.jsx
+++ src/features/movies/movieSlice.jsx
@@ -1,8 +1,23 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/movieApi";
+import { OMDB_API_KEY } from "../../common/constants";
 
 const initialState = {
   movies: [],
+  status: "success", // loading, success, error
 };
+
+export const fetchAsyncMovies = createAsyncThunk("movies/fetchAsyncMovies", async () => {
+  const movieText = "Harry";
+  const response = await movieApi.get(`?apikey=${OMDB_API_KEY}&s=${movieText}&type=movie`);
+  return response.data.Search;
+});
+
+export const fetchAsyncSeries = createAsyncThunk("movies/fetchAsyncSeries", async () => {
+  const movieText = "Friends";
+  const response = await movieApi.get(`?apikey=${OMDB_API_KEY}&s=${movieText}&type=series`);
+  return response.data.Search;
+});
 
 const movieSlice = createSlice({
   name: "movies",
@@ -10,6 +25,20 @@ const movieSlice = createSlice({
   reducers: {
     addMovies: (state, action) => {
       state.movies = action.payload;
+    },
+  },
+  extraReducers: {
+    [fetchAsyncMovies.pending]: (state) => {
+      state.status = "loading";
+      state.movies = [];
+    },
+    [fetchAsyncMovies.fulfilled]: (state, action) => {
+      state.status = "idle";
+      state.movies = action.payload;
+    },
+    [fetchAsyncMovies.rejected]: (state) => {
+      state.status = "error";
+      state.movies = [];
     },
   },
 });
